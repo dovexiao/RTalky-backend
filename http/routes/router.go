50c0737c71539945e75c38297614d1d62e0a7149
RainbowX <yan3260325524@outlook.com>
2025-06-28 19:34:14 +0800

    fix(swagger): 修改swagger环境 - 删除指定的host

--- http/routes/router.go
+++ http/routes/router.go
@@ -22,7 +22,6 @@ import (
 // @contact.email support@swagger.io
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8080
 // @BasePath /api
 func Register(e *echo.Echo) {
 	feature.SetupCORS(e)
